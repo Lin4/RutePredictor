9f4a72d7dfc6593bb4b73a2483c5249553b26f73
Lingeswaran Kandasamy <lingeswarankandasamy@Lingeswarans-iMac.local>
2017-07-17 00:15:27 -0400

    Login UI completed

---
+++ RutePredictor/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  iConnectFinal
+//
+//  Created by Lingeswaran Kandasamy on 5/28/17.
+//  Copyright © 2017 Lingeswaran Kandasamy. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
+let KEY_UID = "uid"
---
+++ RutePredictor/LoginController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RutePredictor
+//
+//  Created by Lingeswaran Kandasamy on 7/16/17.
+//  Copyright © 2017 Lingeswaran Kandasamy. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    @IBOutlet weak var txtName: FancyTextFields!
+    @IBOutlet weak var txtEmaiId: FancyTextFields!
+    @IBOutlet weak var txtPassword: FancyTextFields!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    @IBAction func loginOrRegister(_ sender: Any) {
+    }
+}
+
